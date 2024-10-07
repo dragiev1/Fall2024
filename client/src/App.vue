5eeb3a9103c6aa7f2f6921bb2db1023a5d98a583
dragiev1 <dragievs1@newpaltz.edu>
2024-10-07 14:26:13 -0400

    body and container css in App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,25 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <NavBar />
   </header>
 
-  <RouterView />
+  <div class="container">
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
-
+  .body { 
+    background-color: gray;
+  }
+  .container {
+    background-color: black;
+    box-shadow: drop-shadow(0 0 10px rgb(255, 255, 255, 0.8));
+    min-height: 100vh;
+  }
 </style>
