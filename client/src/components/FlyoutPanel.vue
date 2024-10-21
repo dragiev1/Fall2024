b6eda9a83978ec9d08a988d64b013356863ba809
dragiev1 <dragievs1@newpaltz.edu>
2024-10-21 15:12:11 -0400

    shopping cart

---
+++ client/src/components/FlyoutPanel.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const props = defineProps<{
+    isOpen: boolean
+}>()
+</script>
+
+<template>
+  <div class="flyout" :class="{ isOpen }">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.flyout {
+  position: fixed;
+  top: 56px;
+  right: 0;
+  width: 20rem;
+  height: 100%;
+  background-color: #ffffff7e;
+  box-shadow: 10px 10px 0 rgba(255, 255, 255, 1);
+  z-index: 1000;
+  transform: translate(90%);
+  transition: transform 0.3s ease;
+}
+.flyout:hover,
+.flyout.isOpen {
+  transform: translateX(0);
+}
+</style>
--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,18 +1,26 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from 'vue'
 import { RouterLink } from 'vue-router'
+import FlyoutPanel from './FlyoutPanel.vue'
 
 const isOpen = ref(false)
+const cartIsOpen = ref(false)
 </script>
 
 <template>
-  <nav class="navbar is-info" role="navigation" aria-label="main navigation">
+  <nav class="navbar is-info is-fixed-top" role="navigation" aria-label="main navigation">
     <div class="container">
       <div class="navbar-brand">
         <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="30" height="30" />
 
-        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false"
-           :class="{ 'is-active': isOpen }" @click="isOpen = !isOpen">
+        <a
+          role="button"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          :class="{ 'is-active': isOpen }"
+          @click="isOpen = !isOpen"
+        >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
@@ -25,26 +33,15 @@ const isOpen = ref(false)
           <RouterLink to="/" class="navbar-item">Home</RouterLink>
           <RouterLink to="/products" class="navbar-item">Shop</RouterLink>
 
-
           <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">
-              More
-            </a>
+            <a class="navbar-link"> More </a>
 
             <div class="navbar-dropdown">
-              <RouterLink to="/about" class="navbar-item">
-                About
-              </RouterLink>
-              <RouterLink to="/jobs" class="navbar-item">
-                Jobs
-              </RouterLink>
-              <RouterLink to="/contact" class="navbar-item">
-                Contact
-              </RouterLink>
-              <hr class="navbar-divider">
-              <a class="navbar-item">
-                Report an issue
-              </a>
+              <RouterLink to="/about" class="navbar-item"> About </RouterLink>
+              <RouterLink to="/jobs" class="navbar-item"> Jobs </RouterLink>
+              <RouterLink to="/contact" class="navbar-item"> Contact </RouterLink>
+              <hr class="navbar-divider" />
+              <a class="navbar-item"> Report an issue </a>
             </div>
           </div>
         </div>
@@ -55,20 +52,21 @@ const isOpen = ref(false)
               <a class="button is-primary">
                 <strong>Sign up</strong>
               </a>
-              <a class="button is-light">
-                Log in
-              </a>
+              <a class="button is-light"> Log in </a>
             </div>
           </div>
+          <div class="button" :class="{'is-focused': cartIsOpen}" @click="cartIsOpen = !cartIsOpen"><i class="fas fa-shopping-cart"></i></div>
         </div>
       </div>
     </div>
   </nav>
+  <FlyoutPanel :is-open="cartIsOpen">
+    <h1 class="title">Shopping Cart</h1>
+  </FlyoutPanel>
 </template>
 
 <style scoped>
 .router-link-active {
-
   font-weight: bold;
   border-bottom: 2px solid blue;
 }
